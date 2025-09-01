22e5be7422c389bf2a5492a88566c7b1605aa0b9
DylanCTT <clattdylan@gmail.com>
2025-09-01 17:19:24 -0300

    Añado cosas

--- src/Usuario/Cuenta.java
+++ src/Usuario/Cuenta.java
@@ -1,5 +1,20 @@
 package Usuario;
 
 public class Cuenta {
-
+	String mail;
+	String contraseña;
+	
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	
 }
