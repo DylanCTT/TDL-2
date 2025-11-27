4bc82251fed817e9210a1342e143e5f6a39c5507
DylanCTT <clattdylan@gmail.com>
2025-11-27 17:46:15 -0300

    ventana bienvenida

--- Entregable 3/src/view/VentanaBienvenida.java
+++ Entregable 3/src/view/VentanaBienvenida.java
@@ -1,5 +1,33 @@
 package view;
 
-public class VentanaBienvenida {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
+public class VentanaBienvenida extends JFrame {
+	private JPanel panelContenedor;
+	private CardLayout layout;
+	private JPanel panelPeliculas;
+	private JButton[] botonesCalificar;
+	private ArrayList<JPanel> tarjetasPeliculas = new ArrayList<>();
+
+	public VentanaBienvenida() {
+		setTitle("Plataforma de Streaming");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+
+		// Layout principal con CardLayout
+		layout = new CardLayout();
+		panelContenedor = new JPanel(layout);
+		add(panelContenedor);
+
+		// Vista de espera
+		JPanel panelEspera = new JPanel(new BorderLayout());
+		JLabel mensaje = new JLabel("Cargando películas... Un momento por favor...", SwingConstants.CENTER);
+		mensaje.setFont(new Font("Arial", Font.BOLD, 20));
+		panelEspera.add(mensaje, BorderLayout.CENTER);
+		panelContenedor.add(panelEspera, "espera");
+
+	}
 }
